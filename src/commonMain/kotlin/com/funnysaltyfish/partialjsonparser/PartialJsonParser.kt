06009042e31a194169507a959db067fff777fd47
FunnySaltyFish <3036548996@qq.com>
2023-12-05 22:59:45 +0800

    add some comments

--- src/commonMain/kotlin/com/funnysaltyfish/partialjsonparser/PartialJsonParser.kt
+++ src/commonMain/kotlin/com/funnysaltyfish/partialjsonparser/PartialJsonParser.kt
@@ -7,8 +7,21 @@ object PartialJsonParser {
     /**
      * Parse an incomplete JSON directly, the output should either be [String], [Boolean], [Number], `null`, [List] or [Map]
      *
-     * @param str
-     * @return
+     * Some examples:
+     * ```plain
+     * {"key"                    -> {}
+     * {"key":                   -> {}
+     * {"key":"te                -> {key=te}
+     * {"key":"text", "key2      -> {key=text}
+     * {"key":"text", "key2":"t} -> {key=text, key2=t}}
+     * ```
+     *
+     * Adapted from [Here](https://github.com/SimonTart/json-fragment-parser) by GitHub Copilot
+     *
+     * @param str incomplete JSON string
+     * @return parsed result, normally a [Map] (`{...}`) or [List] (`[...]`)
+     * @throws SyntaxException
+     * @throws JsonParseException
      */
     fun parse(str: String): Any? {
         val tokens = Tokenizer.tokenize(str)
